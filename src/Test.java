095e3575b267f14800693f4f3f418911a734d004
Andrew Towe <atowe@mymail.mines.edu>
2013-11-01 14:20:02 -0600

    Ran first test of Dynamic stuff-Failure

--- src/Test.java
+++ src/Test.java
@@ -1,22 +1,57 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 
 public class Test {
-	public ArrayList<DataStuff> testsubjects;
-	
-	
+	public ArrayList<DataStuff> testsubjects=new ArrayList<DataStuff>();
+	public HashMap<Integer,String> dictionary=new HashMap<Integer, String>();
 	public void run(){
 		getDictionary();
 		getTestSubjects();
 		runTest();
 		getOutput();
-		
+
 	}
 
 	private void runTest() {
+		for(DataStuff d: testsubjects){
+			FileReader file=null;
+			try {
+				file = new FileReader(d.file);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			Scanner reader=new Scanner(file);
+			String next;
+			while(reader.hasNext()){
+				next=reader.next();
+				DynamicPart(next,0,d);
+			}
+		}
+	}
+
+	
+
+	private void DynamicPart(String next, int start, DataStuff d) {
+		if(next.length()==0){
+			d.canwork=false;
+			return;
+		}
+		Integer key=7;
+		for (int i=0; i < next.length(); i++) {
+		    key = key*31+next.charAt(i);
+		};
+		
+		if(dictionary.containsKey(key)){
+			start+=next.length()-1;
+			return;
+		}else{
+			DynamicPart(next.substring(0,next.length()-2), start, d);
+		}
+		
 		
 		
 	}
@@ -30,7 +65,7 @@ public class Test {
 				System.out.println(" is not a corrupted text file and can not be made into words");
 			}
 		}
-		
+
 	}
 
 	public void getTestSubjects() {
@@ -44,12 +79,27 @@ public class Test {
 		while(reader.hasNext()){
 			testsubjects.add(new DataStuff(reader.nextLine()));
 		}
-		
+
 	}
 
 
 	private void getDictionary() {
-		
-		
+		FileReader file=null;
+		try {
+			file = new FileReader("src/Dictionary.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		Scanner reader=new Scanner(file);
+		String word;
+		while(reader.hasNext()){
+			word=reader.next();
+			Integer key=7;
+			for (int i=0; i < word.length(); i++) {
+			    key = key*31+word.charAt(i);
+			};
+			dictionary.put(key,word);
+		}
+
 	}
 }
